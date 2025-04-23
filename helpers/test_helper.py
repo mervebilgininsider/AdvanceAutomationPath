5c47ed3e543bf245f6740bcedbfb67700df1744b
Merve Bilgin <155742507+mervebilgininsider@users.noreply.github.com>
2025-04-23 13:38:13 +0300

    Apply suggestions from code review
    Co-authored-by: Muhammet Furkan DEMİRAL <furkan.demiral@useinsider.com>

--- helpers/test_helper.py
+++ helpers/test_helper.py
@@ -37,3 +37,4 @@ class TestHelper:
             screenshot_path = TestHelper.capture_screenshot(driver, request.node.name)
             TestHelper.add_screenshot_to_report(request, screenshot_path)
             logger.error(f"Test execution failed. Screenshot captured and saved at: {screenshot_path}") 
+            
